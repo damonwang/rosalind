b7dd21900f5afad09cc08fd2cbb2d04461ef7c6e
Damon Wang <damon.wang@gmail.com>
2012-12-25 11:56:43 -0500

    oasis build system, broken

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 eval `opam config -env`
 
-for TARGET in $*
-do
-  ocamlbuild -use-ocamlfind $TARGET -cflags "-w @A-4-33-23"
-done
+# for TARGET in $*
+# do
+#   ocamlbuild -use-ocamlfind $TARGET -cflags "-w @A-4-33-23"
+# done
+
+oasis setup
+ocaml setup.ml -configure
+ocaml setup.ml -build
 
 
